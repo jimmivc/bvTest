182a3307706106b899838a493fc8b9e60f608506
Jimmi <jimmivco@hotmail.com>
2019-01-30 07:10:32 -0600

    code improved

--- spotycloud.go
+++ spotycloud.go
@@ -33,16 +33,11 @@ func newSong() *Song{
 func main() {
 	mux := goji.NewMux()
 	mux.HandleFunc(pat.Get("/"), homepage)
-
-	//mux.HandleFunc(pat.Get("/api/songs"), getAllSongs)
-	//mux.HandleFunc(pat.Get("/api/genres"), getAllGenres)
 	mux.HandleFunc(pat.Get("/api/songs/artist/:artist"), getSongsByArtist)
 	mux.HandleFunc(pat.Get("/api/songs/:name"), getSongsByName)
 	mux.HandleFunc(pat.Get("/api/songs/genre/:genre"), getSongsByGenre)
 	mux.HandleFunc(pat.Get("/api/genres/summary"), getAllGenresSummary)
 	mux.HandleFunc(pat.Get("/api/songs/byLength/:min/:max"), getSongsByLength)
-
-
 
 	log.Fatal(http.ListenAndServe("localhost:8005", mux))
 }
@@ -67,53 +62,9 @@ func initDb() *sql.DB  {
 	return db
 }
 
-func getAllSongs(w http.ResponseWriter,r *http.Request) {
-	w.Header().Set("Content-Type","application/json")
-	db := initDb()
-
-	//var artist string
-	var songs []Song
-
-	rows, _ := db.Query("Select Songs.id,Songs.artist,Songs.song,Genres.id,Genres.name,Songs.length " +
-								"from Songs INNER JOIN Genres On Songs.genre = Genres.id")
-	for rows.Next() {
-		song := newSong()
-
-		err := rows.Scan(&song.Id,&song.Artist,&song.Song,&song.Genre.Id,&song.Genre.Name,&song.Length)
-		if err == nil{
-			songs = append(songs, *song)
-		}
-	}
-
-	json.NewEncoder(w).Encode(songs)
-	db.Close()
-}
-
-func getAllGenres(w http.ResponseWriter,r *http.Request) {
-	w.Header().Set("Content-Type","application/json")
-	db := initDb()
-
-	//var artist string
-	var genres []Genre
-
-	rows, _ := db.Query("select id, name from Genres")
-	for rows.Next() {
-		var genre = new(Genre)
-		err := rows.Scan(&genre.Id,&genre.Name)
-		if err == nil{
-			genres = append(genres, *genre)
-		}
-	}
-
-	json.NewEncoder(w).Encode(genres)
-	db.Close()
-}
-
 func getSongsByArtist(w http.ResponseWriter,r *http.Request){
 	w.Header().Set("Content-Type","application/json")
 	db := initDb()
-
-	var songs []Song
 
 	artist := pat.Param(r, "artist")
 	rows, _ := db.Query("Select  Songs.id,Songs.artist,Songs.song,Genres.id,Genres.name,Songs.length " +
@@ -121,14 +72,8 @@ func getSongsByArtist(w http.ResponseWriter,r *http.Request){
 								"where lower(artist) like lower('%'||?||'%')",artist)
 
 
-	for rows.Next(){
-		song := newSong()
+	songs := mapSongsByQueryResult(rows)
 
-		err := rows.Scan(&song.Id,&song.Artist,&song.Song,&song.Genre.Id,&song.Genre.Name,&song.Length)
-		if err == nil{
-			songs = append(songs, *song)
-		}
-	}
 	if len(songs)>0 {
 		json.NewEncoder(w).Encode(songs)
 	}else {
@@ -142,22 +87,16 @@ func getSongsByName(w http.ResponseWriter,r *http.Request)  {
 	w.Header().Set("Content-Type","application/json")
 	db := initDb()
 
-	var songs []Song
-
 	songName := pat.Param(r, "name")
 	rows, _ := db.Query("Select Songs.id,Songs.artist,Songs.song,Genres.id,Genres.name,Songs.length " +
 		"from Songs INNER JOIN Genres On Songs.genre = Genres.id " +
 		"where lower(song) like lower('%'||?||'%')",songName)
 
 
-	for rows.Next(){
-		song := newSong()
+	songs := mapSongsByQueryResult(rows)
+	fmt.Println(&songs)
+	fmt.Println(songs)
 
-		err := rows.Scan(&song.Id,&song.Artist,&song.Song,&song.Genre.Id,&song.Genre.Name,&song.Length)
-		if err == nil{
-			songs = append(songs, *song)
-		}
-	}
 	if len(songs)>0 {
 		json.NewEncoder(w).Encode(songs)
 	}else {
@@ -169,24 +108,16 @@ func getSongsByName(w http.ResponseWriter,r *http.Request)  {
 func getSongsByGenre(w http.ResponseWriter,r *http.Request){
 	w.Header().Set("Content-Type","application/json")
 	db := initDb()
-	var songs []Song
 	genre := pat.Param(r, "genre")
 	rows, _ := db.Query("Select Songs.id,Songs.artist,Songs.song,Genres.id,Genres.name,Songs.length " +
 		"from Songs INNER JOIN Genres On Songs.genre = Genres.id " +
 		"where lower(Genres.name) like lower('%'||?||'%')",genre)
 
-	for rows.Next(){
-		song := newSong()
-
-		err := rows.Scan(&song.Id,&song.Artist,&song.Song,&song.Genre.Id,&song.Genre.Name,&song.Length)
-		if err == nil{
-			songs = append(songs, *song)
-		}
-	}
+	songs:= mapSongsByQueryResult(rows)
 	if len(songs)>0 {
 		json.NewEncoder(w).Encode(songs)
 	}else {
-		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w,"Not Found")
 	}
 	db.Close()
 }
@@ -222,23 +153,29 @@ func getSongsByLength(w http.ResponseWriter,r *http.Request)  {
 	w.Header().Set("Content-Type","application/json")
 	db := initDb()
 
-	var songs []Song
-
 	min := pat.Param(r,"min")
 	max := pat.Param(r,"max")
 
 	rows, _ := db.Query("Select Songs.id,Songs.artist,Songs.song,Genres.id,Genres.name,Songs.length " +
 								"from Songs INNER JOIN Genres On Songs.genre = Genres.id " +
 								"where length>=? and length<=?",min,max)
+
+	songs := mapSongsByQueryResult(rows)
+
+	json.NewEncoder(w).Encode(songs)
+	db.Close()
+}
+
+func mapSongsByQueryResult(rows *sql.Rows) []Song  {
+	var songs []Song
 	for rows.Next() {
 		song := newSong()
-
 		err := rows.Scan(&song.Id,&song.Artist,&song.Song,&song.Genre.Id,&song.Genre.Name,&song.Length)
 		if err == nil{
 			songs = append(songs, *song)
 		}
 	}
 
-	json.NewEncoder(w).Encode(songs)
-	db.Close()
+	fmt.Println(songs)
+	return songs
 }
