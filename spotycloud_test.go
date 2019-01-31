36d007e80b2885b3cc5b62732e143944cb02dcd1
Shimmi <jimmivco@hotmail.com>
2019-01-31 09:34:26 -0600

    test fixed

--- spotycloud_test.go
+++ spotycloud_test.go
@@ -5,7 +5,6 @@ import (
 	"goji.io/pat"
 	"net/http"
 	"net/http/httptest"
-	"strings"
 	"testing"
 )
 
@@ -14,7 +13,7 @@ func TestFindSongByArtist(t *testing.T)  {
 	mux.HandleFunc(pat.Get("/api/songs/artist/:artist"), getSongsByArtist)
 
 	statusAndResponseGetTest(mux,"/api/songs/artist/The Black Eyed Peas",`{"artist":"The Black Eyed Peas","song":"I Gotta Feeling","genre":{"name":"Rap"},"length":219}`,t)
-	statusAndResponseGetTest(mux,"/api/songs/artist/",`not found`,t)
+	statusAndResponseGetTest(mux,"/api/songs/artist/",``,t)
 
 }
 
@@ -23,7 +22,7 @@ func TestFindSongByName(t *testing.T)  {
 	mux.HandleFunc(pat.Get("/api/songs/:name"), getSongsByName)
 
 	statusAndResponseGetTest(mux,"/api/songs/Macarena",`{"artist":"Los Del Rio","song":"Macarena","genre":{"name":"Pop"},"length":159}`,t)
-	statusAndResponseGetTest(mux,"/api/songs/",`not found`,t)
+	statusAndResponseGetTest(mux,"/api/songs/",``,t)
 }
 
 func TestFindSongByGenre(t *testing.T)  {
@@ -31,7 +30,7 @@ func TestFindSongByGenre(t *testing.T)  {
 	mux.HandleFunc(pat.Get("/api/songs/genre/:genre"), getSongsByGenre)
 
 	statusAndResponseGetTest(mux,"/api/songs/genre/latin pop rock",`{"artist":"Los Waldners","song":"Horacio","genre":{"name":"Latin Pop Rock"},"length":165}`,t)
-	statusAndResponseGetTest(mux,"/api/songs/genre/",`not found`,t)
+	statusAndResponseGetTest(mux,"/api/songs/genre/",``,t)
 }
 
 func TestListGenresSummary(t *testing.T)  {
@@ -47,10 +46,10 @@ func TestFindSongByLength(t *testing.T)  {
 	mux := goji.NewMux()
 	mux.HandleFunc(pat.Get("/api/songs/byLength/:min/:max"), getSongsByLength)
 	statusAndResponseGetTest(mux,"/api/songs/byLength/180/190",`{"artist":"LMFAO","song":"Party Rock Anthem","genre":{"name":"Rap"},"length":189}`,t)
-	statusAndResponseGetTest(mux,"/api/songs/byLength/asd/fds",`Input format error`,t)
-	statusAndResponseGetTest(mux,"/api/songs/byLength/100/fds",`Input format error`,t)
-	statusAndResponseGetTest(mux,"/api/songs/byLength/asd/150",`Input format error`,t)
-	statusAndResponseGetTest(mux,"/api/songs/byLength/190/180",`Invalid Range (min/max)`,t)
+	statusAndResponseGetTest(mux,"/api/songs/byLength/asd/fds",`{Input format error}`,t)
+	statusAndResponseGetTest(mux,"/api/songs/byLength/100/fds",`{Input format error}`,t)
+	statusAndResponseGetTest(mux,"/api/songs/byLength/asd/150",`{Input format error}`,t)
+	statusAndResponseGetTest(mux,"/api/songs/byLength/190/180",`{Invalid Range (min/max)}`,t)
 
 }
 
@@ -68,15 +67,16 @@ func statusAndResponseGetTest(mux *goji.Mux,path string, expected string,t *test
 	// directly and pass in our Request and ResponseRecorder.
 	mux.ServeHTTP(rr,req)
 
-	if expected
 	// Check the status code is what we expect.
 	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
+		if status := rr.Code; status != http.StatusNotFound && rr.Body.String()!=expected {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusOK)
+		}
 	}
 
-	if !strings.Contains(rr.Body.String(),expected) {
-		t.Errorf("handler returned unexpected body: got \n%v want \n%v",
+	if rr.Body.String()==expected {
+		t.Errorf("handler returned unexpected body: got |%v| want |%v|",
 			rr.Body.String(), expected)
 	}
 }
